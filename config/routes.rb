33199462c6d5df08f2dd897b4bc2ef627e356cfd
Katie Carrel <katiecarrel@comcast.net>
2019-07-07 20:20:04 -0700

    posting tweets to backend db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :tweets
-      get '/tweet', to: 'tweets#tweet'
+  resources :tweets, only: [:post]
+      post '/tweet', to: 'tweets#tweet'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
